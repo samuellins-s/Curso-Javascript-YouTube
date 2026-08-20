7898d6e6cebb22498341ab6b090f4f5aa2362778
samuellins <rnsamueldlins@gmail.com>
2026-08-20 16:11:04 -0300

    aulas 03 js

---
+++ aula03 - ifs/aula03#EX.js
@@ -0,0 +1,15 @@
+/*
+
+Utilizando IF/ELSE,
+    verificar se uma cidade é grande ou pequena
+    cidade grande possui mais de 300mil habitantes
+
+*/
+
+const habitantes = 60000
+
+if (habitantes > 300000){
+    console.log(`A sua cidade é GRANDE! Possui ${habitantes} habitantes!`)
+} else {
+    console.log(`A sua cidade é PEQUENA! Possui ${habitantes} habitantes! `)
+}
---
+++ aula03 - ifs/aula03.js
@@ -0,0 +1,14 @@
+/*
+
+== igual que (compara SOMENTE o valor)
+=== igual que (compara o VALOR e o TIPO)
+
+*/
+
+const nota_aluno = 7
+
+if(nota_aluno >= 6){
+   console.log(`Aluno APROVADO!!! Nota: ${nota_aluno}`) 
+} else {
+    console.log(`Aluno REPROVADO!!! Nota: ${nota_aluno}`)
+}
